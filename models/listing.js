c91a47c593300a6547535a052f0c166ee969b662
Kundan Gupta <Kg369033@gmail.com>
2025-06-21 01:24:07 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,64 @@
+const mongoose =require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const listingSchema =new Schema({
+    title: {type:String,required:true},
+    description: {type:String},
+    
+    
+    image: {
+        filename: {
+          type: String,
+          default: "default",
+        },
+        url: {
+          type: String,
+          default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
+          set: (v) =>
+            !v || v.trim() === ""
+              ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+              : v,
+        }
+      }
+      ,
+    
+    price: {type:Number},
+    location: {type:String},
+    country:String
+});
+const Listing = mongoose.model("listing",listingSchema);
+module.exports = Listing;  //export the model
+
+
+
+// image: {
+    //     type:String,
+    //     default:
+    //         "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfH",
+    //     set: (v) =>
+    //      v===""
+    //      ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfH":v,
+    // },
+
+
+    // sahi wala :
+    // image: {
+    //     filename: String,
+    //     url: {
+    //         type: String,
+    //         default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
+    //         set : (v) => v===" "? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfH":v,
+
+    //     }
+    // },
+
+// image: {
+//         filename: String,
+//         url: {
+//             type: String,
+//             default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
+//             set : (v) => v==" "? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfH":v,
+
+//         }
+//     },
